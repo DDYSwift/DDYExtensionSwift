3b7bc0183554f17ab73bf1a35a65fc9384c8696d
dou <634778311@qq.com>
2019-07-28 22:19:31 +0800

    Create DDYConst.swift

---
+++ DDYExtensionSwift/DDYExtension/DDYConst.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+// @_exported import Hue
+// @_exported import RxSwift
+// @_exported import RxCocoa
+// @_exported import Rswift
+// @_exported import Then
+
+// @_exported import IQKeyboardManagerSwift
+// @_exported import Alamofire
+// @_exported import Moya
+// @_exported import Kingfisher
+// @_exported import SnapKit
+// @_exported import SwiftyJSON
+// @_exported import ObjectMapper
+
+/// RxSwift 回收池
+// let disposeBag = DisposeBag()
+
+/// MainScreen Bounds
+let DDYScreenBounds = UIScreen.main.bounds
+/// MainScreen Size
+let DDYScreenSize = UIScreen.main.bounds.size
+/// 屏幕宽度 UIScreen.main.bounds.size.width
+ let DDYScreenW = UIScreen.main.bounds.width
+/// 屏幕高度
+ let DDYScreenH = UIScreen.main.bounds.height
+/// 屏幕缩放值
+let DDYScreenScale = UIScreen.main.scale
+/// 状态条高度
+let DDYStatusH = UIApplication.shared.statusBarFrame.height
