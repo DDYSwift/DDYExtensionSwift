37a01d5547c13cc2c0cb26c5a732a92263fe82cf
rain <doudianyu@idengyun.com>
2019-07-29 17:41:42 +0800

    添加按钮图文样式

--- DDYExtensionSwift/DDYExtension/DDYButtonExtension.swift
+++ DDYExtensionSwift/DDYExtension/DDYButtonExtension.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 public enum DDYButtonStyle: Int {
+    case defaultStyle   = 0 // 默认效果(为了处理无调用状态，不可赋值)
     case imageLeft      = 1 // 左图右文
     case imageRight     = 2 // 右图左文
     case imageTop       = 3 // 上图下文
@@ -11,6 +12,32 @@ private var styleKey: Void?
 private var paddingKey: Void?
 
 extension DDYWrapperProtocol where DDYT : UIButton {
+    /// 设置图文样式(不可逆，一旦设置不能百分百恢复系统原来样式)
+    var style: DDYButtonStyle {
+        get {
+            guard let style = objc_getAssociatedObject(ddyValue, &styleKey) as? DDYButtonStyle else {
+                return .defaultStyle
+            }
+            return style
+        }
+        set {
+            objc_setAssociatedObject(ddyValue, &styleKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            ddyValue.layoutIfNeeded()
+        }
+    }
+
+    var padding: CGFloat {
+        get {
+            guard let padding = objc_getAssociatedObject(ddyValue, &paddingKey) as? CGFloat else {
+                return 0.5
+            }
+            return padding
+        }
+        set {
+            objc_setAssociatedObject(ddyValue, &paddingKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            ddyValue.layoutIfNeeded()
+        }
+    }
 
     public func setBackgroundColor(_ color: UIColor?, for state: UIControl.State) {
         guard let color = color else {
@@ -30,53 +57,55 @@ extension DDYWrapperProtocol where DDYT : UIButton {
     }
 }
 
+extension UIButton {
+    public static func ddySwizzleMethod() {
+        ddySwizzle(#selector(layoutSubviews), #selector(ddyLayoutSubviews), swizzleClass: self)
+    }
 
-final class DDYButton: UIButton {
-    /// 布局样式
-    var style: DDYButtonStyle = .imageLeft
-    /// 图文间距
-    var padding: CGFloat = 0.5
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
+    @objc private func ddyLayoutSubviews() {
+        self.ddyLayoutSubviews()
         adjustRect(margin: (contentEdgeInsets.top, contentEdgeInsets.left, contentEdgeInsets.bottom, contentEdgeInsets.right))
+
     }
 
     private func adjustRect(margin:(top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat)) {
-        guard let imageSize = imageView?.frame.size, let titleSize = titleLabel?.frame.size else {
+        guard let imageSize = self.imageView?.frame.size, let titleSize = self.titleLabel?.frame.size else {
             return
         }
         guard imageSize != CGSize.zero && titleSize != CGSize.zero else {
             return
         }
         func horizontal(_ leftView: UIView,_ rightView: UIView) {
-            let contentW = max(leftView.frame.width + padding + rightView.frame.width, frame.width-margin.left-margin.right)
-            let contentH = max(max(leftView.frame.height, rightView.frame.height), frame.height-margin.top-margin.bottom)
+            let contentW = leftView.frame.width + self.ddy.padding + rightView.frame.width
+            let contentH = max(leftView.frame.height, rightView.frame.height)
             let leftOrigin = CGPoint(x: margin.left, y: (contentH-leftView.frame.height)/2.0 + margin.top)
-            let rightOrigin = CGPoint(x: margin.left + leftView.frame.width + padding, y: (contentH-rightView.frame.height)/2.0 + margin.top)
+            let rightOrigin = CGPoint(x: margin.left + leftView.frame.width + self.ddy.padding, y: (contentH-rightView.frame.height)/2.0 + margin.top)
 
-            bounds = CGRect(x: 0, y: 0, width: contentW + margin.left + margin.right, height: contentH + margin.top + margin.bottom)
+            self.bounds = CGRect(x: 0, y: 0, width: contentW + margin.left + margin.right, height: contentH + margin.top + margin.bottom)
             leftView.frame = CGRect(origin: leftOrigin, size: leftView.frame.size)
             rightView.frame = CGRect(origin: rightOrigin, size: rightView.frame.size)
         }
-        func vertical(_ topView: UIView,_ bottomView: UIView) {
-            let contentW = max(max(topView.frame.width, bottomView.frame.width), frame.width-margin.left-margin.right)
-            let contentH = max(topView.frame.height + padding + bottomView.frame.height, frame.height-margin.top-margin.bottom)
+        func vertical(_ topView: UIView,_ bottomView: UIView,_ backSize: CGSize) {
+            let contentW = max(max(topView.frame.width, bottomView.frame.width), backSize.width-margin.left-margin.right)
+            let contentH = max(topView.frame.height + self.ddy.padding + bottomView.frame.height, backSize.height-margin.top-margin.bottom)
             let topOrigin = CGPoint(x: (contentW-topView.frame.width)/2.0 + margin.left, y: margin.top)
-            let bottomOrigin = CGPoint(x: (contentW-bottomView.frame.width)/2.0 + margin.left, y: margin.top + topView.frame.height + padding)
+            let bottomOrigin = CGPoint(x: (contentW-bottomView.frame.width)/2.0 + margin.left, y: margin.top + topView.frame.height + self.ddy.padding)
 
-            bounds = CGRect(x: 0, y: 0, width: contentW + margin.left + margin.right, height: contentH + margin.top + margin.bottom)
+            self.bounds = CGRect(x: 0, y: 0, width: contentW + margin.left + margin.right, height: contentH + margin.top + margin.bottom)
             topView.frame = CGRect(origin: topOrigin, size: topView.frame.size)
             bottomView.frame = CGRect(origin: bottomOrigin, size: bottomView.frame.size)
+            print("layout: \(self.bounds) \(topView.frame) \(bottomView.frame)")
         }
 
+        print("0000: \(self.bounds) \(self.imageView!.frame) \(self.titleLabel!.frame)")
         titleLabel?.sizeToFit()
-        switch style {
-        case .imageLeft: horizontal(imageView!, titleLabel!)
-        case .imageRight: horizontal(titleLabel!, imageView!)
-        case .imageTop: vertical(imageView!, titleLabel!)
-        case .imageBottom: vertical(titleLabel!, imageView!)
+        switch self.ddy.style {
+        case .imageLeft: horizontal(self.imageView!, self.titleLabel!)
+        case .imageRight: horizontal(self.titleLabel!, self.imageView!)
+        case .imageTop: vertical(self.imageView!, self.titleLabel!, self.frame.size)
+        case .imageBottom: vertical(self.titleLabel!, self.imageView!, self.frame.size)
+        case .defaultStyle: return
+        default: return
         }
-        print("\(bounds) \(titleLabel!.frame) \(imageView!.frame)")
     }
 }
